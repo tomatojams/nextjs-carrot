44d93c0b4f947b51ba30ffc56b3aadf48ea47ac1
tomatojams <tomatojams@naver.com>
2024-11-28 03:17:42 +0900

    인풋체크

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,36 +1,33 @@
+"use client";
+
 import FormButton from "@/app/components/form-btn";
 import FormInput from "@/app/components/from-input";
 import SocialLogin from "@/app/components/social-login";
-import {  useFormStatus } from "react-dom";
 
+import { useActionState } from "react";
+
+import { handleForm } from "./actions";
 export default function LogIn() {
-  const handleForm = async (formData: FormData) => {
-      "use server";
-      await new Promise(resolve => setTimeout(resolve, 2000));
-   
-    console.log("loggied in");
-  };
-    useFormStatus();
+
+  // 서버액션을 클라이언트에서 사용하는 훅
+  const [state, action] = useActionState(handleForm,null);
+
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with email and password.</h2>
       </div>
-      <form action={handleForm} className="flex flex-col gap-3">
-        <FormInput
-          name="email"
-          type="email"
-          placeholder="Email"
-          required
-          errors={[]}
-        />
+      {/* 서버액션 트리거 */}
+      <form action={action} className="flex flex-col gap-3">
+        <FormInput name="email" type="email" placeholder="Email" required errors={[]} />
         <FormInput
           name="password"
           type="password"
           placeholder="Password"
           required
-          errors={[]}
+          // 서버액션 스테이트
+          errors={[state?.errors ?? []]}
         />
         <span>
           <FormButton text="Log in" />
